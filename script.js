c55fe7c012637ed6b37d6100292703c00f6fc190
tamsay <longetemitope@gmail.com>
2020-04-17 07:36:14 +0100

    More styling done on the homepage

--- script.js
+++ script.js
@@ -15,8 +15,9 @@ tabs.forEach(tab => {
   })
 })
 
-const key = "e6ef2cde327f46e3820d0344025b79fc"
-const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${key}`
+// const key = "e6ef2cde327f46e3820d0344025b79fc"
+// const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${key}`
+
 
 const recievedNews = (newsdata) => {
     const articlesDiv = document.querySelector("#newsFeed")
@@ -26,15 +27,23 @@ const recievedNews = (newsdata) => {
       const div = document.createElement("div")
       div.className = "news"
       div.innerHTML = `
-			<h2>${article.title}</h2>
+      <h2>${article.title}</h2>
 			<img src="${article.urlToImage}"/>`
       articlesDiv.appendChild(div)
-			
     })
 }
 
+var url = 'http://newsapi.org/v2/top-headlines?' +
+  'country=us&' +
+  'apiKey=95652013248a41419ede93837e41b30d';
 //Fetch sends a request to the API.
 //Promise makes it possible to run this in the background. När vi får APIet då går den vidare och skickar tillbaka JSON.
 fetch(url)
   .then(response => response.json())
   .then(recievedNews)
+
+var req = new Request(url);
+fetch(req)
+.then(function(response) {
+console.log(response.json());
+})
