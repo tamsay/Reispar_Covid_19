740e15c640008796b86e61df89651595273f4207
tamsay <longetemitope@gmail.com>
2020-04-17 19:49:15 +0100

    Google news feed completed and addition of another powerbi module

--- script.js
+++ script.js
@@ -16,9 +16,10 @@ tabs.forEach(tab => {
 })
 
 const recievedNews = (newsdata) => {
+
     const articlesDiv = document.querySelector("#newsFeed")
-    newsdata.articles.forEach((article) => {
-			
+
+    let  createArticle=(article, activeClass)=>{	
       const div = document.createElement("div");
       const title = document.createElement('h2');
       const image = document.createElement('img')
@@ -26,8 +27,7 @@ const recievedNews = (newsdata) => {
       const imageLink = document.createElement('a')
       const urlParagraph = document.createElement('p');
 
-      div.className = "news"
-      image.className = 'imageLink'
+      div.className = activeClass
       
       url.setAttribute('href', `${article.url}`)
       url.setAttribute('target', '_blank')
@@ -38,7 +38,8 @@ const recievedNews = (newsdata) => {
 
       title.innerText = `${article.title}`
 
-      image.setAttribute('src', `${article.image}`);
+      image.className = 'imageLink img-fluid mx-auto d-block'
+      image.setAttribute('src', `${article.urlToImage}`);
       imageLink.appendChild(image);
       imageLink.setAttribute('href', `${article.url}`)
       imageLink.setAttribute('target', '_blank')
@@ -47,13 +48,66 @@ const recievedNews = (newsdata) => {
       div.appendChild(urlParagraph);
       div.appendChild(imageLink);
       articlesDiv.appendChild(div)
-    })
+    };
+
+    for(let x=0; x < newsdata.articles.length; x++){
+      let article = newsdata.articles[x];
+      
+      if( x === 0){
+        console.log('index 0');
+        let name = "news carousel-item col-12 col-sm-6 col-md-4 col-lg-3 active"
+        createArticle(article, name)
+      }
+      else{
+        let name = "news carousel-item col-12 col-sm-6 col-md-4 col-lg-3"
+        createArticle(article,name)
+      }
+    };
 }
 
-fetch('https://gnews.io/api/v3/search?q=example&token=b0f2424c549bcfcad8558f929836b197')
+// fetch('https://gnews.io/api/v3/search?q=covid-19 | coronavirus?&token=b0f2424c549bcfcad8558f929836b197')
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data)
+//         recievedNews(data)
+//     });
+
+    fetch('    https://newsapi.org/v2/everything?q=covid-19 AND coronavirus&apiKey=95652013248a41419ede93837e41b30d')
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
+      console.log(data)
         recievedNews(data)
     });
+
+
+
+
+    /*
+    Carousel
+*/
+$('#carousel-example').on('slide.bs.carousel', function (e) {
+  /*
+      CC 2.0 License Iatek LLC 2018 - Attribution required
+  */
+  var $e = $(e.relatedTarget);
+  var idx = $e.index();
+  var itemsPerSlide = 5;
+  var totalItems = $('.carousel-item').length;
+
+  if (idx >= totalItems-(itemsPerSlide-1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i=0; i<it; i++) {
+          // append slides to end
+          if (e.direction=="left") {
+              $('.carousel-item').eq(i).appendTo('.carousel-inner');
+          }
+          else {
+              $('.carousel-item').eq(0).appendTo('.carousel-inner');
+          }
+      }
+  }
+});
