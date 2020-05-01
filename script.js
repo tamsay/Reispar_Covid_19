11615c5d082e440607c6c86822852575892f840f
tamsay <longetemitope@gmail.com>
2020-05-01 23:39:44 +0100

    styling and refactoring

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ const recievedNews = (newsdata) => {
     };
 }
 
-    fetch('    https://newsapi.org/v2/everything?q=covid-19 AND coronavirus&pageSize=8&apiKey=95652013248a41419ede93837e41b30d')
+    fetch('    https://newsapi.org/v2/everything?q=covid-19 AND coronavirus&pageSize=20&apiKey=95652013248a41419ede93837e41b30d')
     .then(function (response) {
         return response.json();
     })
